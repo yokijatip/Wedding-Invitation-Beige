e76228dae77991fa68ba57df6fde365867840f81
Yoki Jati Perkasa <yokijati@gmail.com>
2025-05-24 21:48:47 +0700

    Fixing footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="bg-[#111111] text-slate-200">
+  <div class="bg-[#111111]">
     <section class="flex flex-row justify-between items-center py-2 px-2">
-      <h1 class="font-cursive font-light text-[0.8rem]">GITY Headquarters</h1>
-      <h1 class="text-[0.8rem] font-light">
+      <h1 class="font-cursive font-light text-[0.8rem] text-gray-400">
+        <span class="font-bold text-gray-50">GITY</span> Headquarters
+      </h1>
+      <h1 class="text-[0.8rem] font-light text-gray-400">
         Designed by
-        <span class="font-bold font-new-icon tracking-widest"
-          >Yoki J Perkasa</span
-        >
+        <span class="font-bold font-tenor text-gray-50">Yoki J Perkasa</span>
       </h1>
     </section>
   </div>
